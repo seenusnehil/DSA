53a175d3fa5ae2f132aa3d3df1be47e0da9596fa
Snehil Seenu <snehilseenu@gmail.com>
2023-08-13 17:15:53 +0530

    revision

--- DSA All questions (Striver sheet)/Arrays/rightRotateArray.cpp
+++ DSA All questions (Striver sheet)/Arrays/rightRotateArray.cpp
@@ -1,21 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void rightRotateArray(int arr[], int n, int k) {
-    int temp[n];
+// void rightRotateArray(int arr[], int n, int k) {
+//     int temp[n];
+//     for(int i=0;i<n;i++) {
+
+//         int rotate = ((i-k);
+//         if(rotate<0) {
+//             rotate += n;
+//         })
+
+//         temp[i] = arr[(rotate%n)];
+//     }
+
+//     for(int i=0;i<n;i++) {
+//         cout<<temp[i]<<" ";
+//     }   
+// }
+
+void rightRotateArray2(int arr[], int n, int k) {
+    
+    reverse(arr, arr+n);
+
+    reverse(arr, arr+n-k-1);
+
+    reverse(arr+k, arr+n);
+
     for(int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
+    }  
 
-        int rotate = ((i-k);
-        if(rotate<0) {
-            rotate += n;
-        })
-
-        temp[i] = arr[(rotate%n)];
-    }
-
-    for(int i=0;i<n;i++) {
-        cout<<temp[i]<<" ";
-    }   
 }
 
 int main() {
@@ -31,6 +45,6 @@ int main() {
     int k;
     cin>>k;
 
-    rightRotateArray(arr, n, k);
+    rightRotateArray2(arr, n, k);
     return 0;
 }
